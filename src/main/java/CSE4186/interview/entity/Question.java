a9ce624ef6a5c431b4a086b1072a07325306ebd6
syh24 <dbsgur6896@naver.com>
2024-03-20 21:03:40 +0900

    Mysql연동 및 초기 Entity추가

---
+++ src/main/java/CSE4186/interview/entity/Question.java
@@ -0,0 +1,31 @@
+package CSE4186.interview.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "self_introduction_id")
+    private SelfIntroduction selfIntroduction;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+}
