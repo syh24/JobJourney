b6c4c54049eaa1c6dc9ec4668f976e2efa3d9e8b
syh <dbsgur6896@naver.com>
2024-03-26 13:28:24 +0900

    게시판 기능 추가

--- src/main/java/CSE4186/interview/entity/Comment.java
+++ src/main/java/CSE4186/interview/entity/Comment.java
@@ -2,6 +2,7 @@ package CSE4186.interview.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -29,8 +30,8 @@ public class Comment {
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "board_id")
-    private Board board;
+    @JoinColumn(name = "post_id")
+    private Post post;
 
     @CreatedDate
     @Column(name = "created_at")
@@ -42,4 +43,12 @@ public class Comment {
 
     @Column(name = "deletedAt")
     private LocalDateTime deletedAt;
+
+    @Builder
+    public Comment(Long id, String content, User user, Post post) {
+        this.id = id;
+        this.content = content;
+        this.user = user;
+        this.post = post;
+    }
 }
