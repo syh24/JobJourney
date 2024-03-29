6083be9afecf8783bb38f6705e27915d91ed03e2
syh <dbsgur6896@naver.com>
2024-03-29 23:33:33 +0900

    Revert "cors port 변경"
    This reverts commit 9259e6794b311a45725c5c64baf16df996dbb570.

--- src/main/java/CSE4186/interview/config/WebConfig.java
+++ src/main/java/CSE4186/interview/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("http://localhost:9000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods(HttpMethod.GET.name());
     }
 }
