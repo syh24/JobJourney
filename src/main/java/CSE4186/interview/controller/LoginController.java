8d6b8379b4e0d796475bb6a01ac19d8cf677859c
syh24 <dbsgur6896@naver.com>
2024-04-03 16:34:40 +0900

    :art: Schema 이름 추가

--- src/main/java/CSE4186/interview/controller/LoginController.java
+++ src/main/java/CSE4186/interview/controller/LoginController.java
@@ -4,19 +4,25 @@ package CSE4186.interview.controller;
 import CSE4186.interview.controller.dto.BaseResponseDto;
 import CSE4186.interview.controller.dto.UserDTO;
 import CSE4186.interview.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@Tag(name = "Login", description = "Login API")
 public class LoginController {
 
     private final UserService userService;
 
     @PostMapping("/join")
+    @Operation(summary = "Join User", description = "회원가입")
     public ResponseEntity<BaseResponseDto<String>> join(@RequestBody UserDTO.joinRequest request){
         userService.join(request);
         return ResponseEntity.ok(
@@ -28,6 +34,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Login", description = "로그인")
     public ResponseEntity<BaseResponseDto<String>> login(@RequestBody UserDTO.loginRequest request){
 
         if(!userService.login(request)){
--- src/main/java/CSE4186/interview/controller/dto/UserDTO.java
+++ src/main/java/CSE4186/interview/controller/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package CSE4186.interview.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 public class UserDTO {
@@ -8,6 +9,7 @@ public class UserDTO {
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
+    @Schema(name = "userJoinRequest", description = "회원가입 Request DTO")
     public static class joinRequest{
         private String name;
         private String email;
@@ -18,6 +20,7 @@ public class UserDTO {
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
+    @Schema(name = "userLoginRequest", description = "로그인 Request DTO")
     public static class loginRequest{
         private String email;
         private String password;
