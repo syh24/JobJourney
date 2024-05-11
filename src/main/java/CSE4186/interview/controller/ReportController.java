00dc60b3f4a5aa8826e96ff2b2cbb799732c9310
syh <dbsgur6896@naver.com>
2024-05-12 00:23:16 +0900

    :sparkles: 게시글, 댓글 신고하기 기능 추가

---
+++ src/main/java/CSE4186/interview/controller/ReportController.java
@@ -0,0 +1,29 @@
+package CSE4186.interview.controller;
+
+import CSE4186.interview.controller.dto.ReportDto;
+import CSE4186.interview.service.ReportService;
+import CSE4186.interview.utils.ApiUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/report")
+@Tag(name = "Report", description = "Report API")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping
+    @Operation(summary = "Create Report", description = "신고하기")
+    public ApiUtil.ApiSuccessResult<Long> addReport(@Valid @RequestBody ReportDto.createRequest request) {
+        Long reportId = reportService.addReport(request);
+        return ApiUtil.success(reportId);
+    }
+}
