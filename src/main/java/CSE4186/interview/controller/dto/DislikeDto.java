5380b32887175380aa7d30bdfa3eb4fa65245ec6
syh <dbsgur6896@naver.com>
2024-05-12 17:14:41 +0900

    :sparkles: 게시글 좋아요/싫어요 기능 추가

---
+++ src/main/java/CSE4186/interview/controller/dto/DislikeDto.java
@@ -0,0 +1,19 @@
+package CSE4186.interview.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class DislikeDto {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Schema(name = "dislikeCreateRequest", description = "싫어요 DTO")
+    public static class creteRequest {
+        @NotNull
+        private Long userId;
+    }
+}
