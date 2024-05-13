7404d5c1b6c9e60ed53d7d99bf4d74e4afe22cf7
3000minsu <2000minsu@naver.com>
2024-05-13 16:24:38 +0900

    연동 테스트

--- src/main/java/CSE4186/interview/service/TextToSpeechService.java
+++ src/main/java/CSE4186/interview/service/TextToSpeechService.java
@@ -1,16 +1,22 @@
 package CSE4186.interview.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TextToSpeechService {
 
-    public byte[] synthesizeText(String text) throws IOException {
+    /*public byte[] synthesizeText(String text) throws IOException {
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
             // Set the text input to be synthesized
             SynthesisInput input = SynthesisInput.newBuilder()
@@ -35,5 +41,48 @@ public class TextToSpeechService {
             ByteString audioContents = response.getAudioContent();
             return audioContents.toByteArray();
         }
+    }*/
+    private final String apiKey;
+    public TextToSpeechService(@Value("${google.api-key}") String secret){
+        apiKey=secret;
+        //this.objectMapper = objectMapper;
+        //this.textToSpeechService = textToSpeechService;
+    }
+    //private static final String GOOGLE_TTS_API_URL = "https://texttospeech.googleapis.com/v1/text:synthesize?key=";
+
+    //@PostMapping("/tts")
+    public byte[] convertTextToSpeech(@RequestBody String text) {
+        String GOOGLE_TTS_API_URL = "https://texttospeech.googleapis.com/v1/text:synthesize?key=" + apiKey;
+        /*RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestJson = "{\"input\":{\"text\":" + text + "},\"voice\":{\"languageCode\":\"ko-KR\",\"name\":\"ko-KR-Neural2-c\", \"ssmlGender\":\"MALE\"},\"audioConfig\":{\"audioEncoding\":\"MP3\"}}";
+        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+        ResponseEntity<byte[]> response = restTemplate.exchange(GOOGLE_TTS_API_URL, HttpMethod.POST, entity, byte[].class);
+        return response.getBody();*/
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> requestMap = new HashMap<>();
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("text", text);
+
+        Map<String, String> voiceMap = new HashMap<>();
+        voiceMap.put("languageCode", "ko-KR");
+        voiceMap.put("name", "ko-KR-Neural2-C");
+
+        Map<String, String> audioConfigMap = new HashMap<>();
+        audioConfigMap.put("audioEncoding", "MP3");
+
+        requestMap.put("input", inputMap);
+        requestMap.put("voice", voiceMap);
+        requestMap.put("audioConfig", audioConfigMap);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestMap, headers);
+        ResponseEntity<byte[]> response = restTemplate.exchange(GOOGLE_TTS_API_URL, HttpMethod.POST, entity, byte[].class);
+
+
+        return response.getBody();
     }
 }
