a431d9b8dd31e54abb9ed05ff9ab86b77dd5b5b5
sohye0n <sbb0416@naver.com>
2024-05-19 11:12:52 +0900

    securityFilter에서 에러핸들링

--- src/main/java/CSE4186/interview/jwt/JwtFilter.java
+++ src/main/java/CSE4186/interview/jwt/JwtFilter.java
@@ -1,10 +1,12 @@
 package CSE4186.interview.jwt;
 
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -57,47 +59,46 @@ public class JwtFilter extends OncePerRequestFilter {
         //1. accessToken을 가지는지
         if(StringUtils.hasText(accessToken)){
             //2. accessToken이 유효하다면 pass
-            if(tokenProvider.validateToken(accessToken,request)){
-                //2.1 securityContext에 저장한다.
-                SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(accessToken));
-            }
+                if(tokenProvider.validateToken(accessToken,request)){
+                    //2.1 securityContext에 저장한다.
+                    SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(accessToken));
+                }
 
-            //2. accessToken이 유효하지 않다면 refreshToken의 유효성 검사
-            else{
-                //2.1 accessToken의 유효기간이 지난 경우
-                logger.info(String.valueOf(request.getAttribute("exception")));
-                if(String.valueOf(request.getAttribute("exception")).equals("expired token.")){
-                    //2.2 refreshToken을 가진다면
-                    String refreshToken=resolveRefreshToken(request);
-                    if(refreshToken!=null){
-                        //2.3 refreshToken이 유효하다면 & 서버에서 발급한 토큰이 맞다면
-                        if(tokenProvider.validateToken(refreshToken,request) && tokenProvider.checkRefreshToken(refreshToken)){
-                            //2.4 새로운 accessToken을 발급해준다
-                            accessToken= tokenProvider.createAccessToken(tokenProvider.getAuthentication(refreshToken));
+                //2. accessToken이 유효하지 않다면 refreshToken의 유효성 검사
+                else{
+                    //2.1 accessToken의 유효기간이 지난 경우
+                    if(String.valueOf(request.getAttribute("exception")).equals("expired token.")){
+                        //2.2 refreshToken을 가진다면
+                        String refreshToken=resolveRefreshToken(request);
+                        if(refreshToken!=null){
+                            //2.3 refreshToken이 유효하다면 & 서버에서 발급한 토큰이 맞다면
+                            if(tokenProvider.validateToken(refreshToken,request) && tokenProvider.checkRefreshToken(refreshToken)){
+                                //2.4 새로운 accessToken을 발급해준다
+                                accessToken= tokenProvider.createAccessToken(tokenProvider.getAuthentication(refreshToken));
 
-                            //2.5 새로운 refreshToken을 발급해준다
-                            refreshToken= tokenProvider.createRefreshToken(tokenProvider.getAuthentication(refreshToken),refreshToken);
+                                //2.5 새로운 refreshToken을 발급해준다
+                                refreshToken= tokenProvider.createRefreshToken(tokenProvider.getAuthentication(refreshToken),refreshToken);
 
-                            //2.6 accessToken을 헤더에 담는다.
-                            response.setHeader(AUTHORIZATION_HEADER,"Bearer "+accessToken);
+                                //2.6 accessToken을 헤더에 담는다.
+                                response.setHeader(AUTHORIZATION_HEADER,"Bearer "+accessToken);
 
-                            //2.7 refreshToken을 쿠키에 담는다.
-                            response.setHeader("Set-Cookie",
-                                    "refreshToken="+refreshToken+"; "+
-                                            "Path=/;" +
-                                            "Domain=localhost; " +
-                                            "HttpOnly; " +
-                                            "Max-Age=604800; "
-                            );
+                                //2.7 refreshToken을 쿠키에 담는다.
+                                response.setHeader("Set-Cookie",
+                                        "refreshToken="+refreshToken+"; "+
+                                                "Path=/;" +
+                                                "Domain=localhost; " +
+                                                "HttpOnly; " +
+                                                "Max-Age=604800; "
+                                );
 
-                            //2.8 securityContext에 저장한다
-                            SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(refreshToken));
+                                //2.8 securityContext에 저장한다
+                                SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(refreshToken));
+                            }
                         }
                     }
                 }
             }
-        }
-
         filterChain.doFilter(request,response);
     }
+
 }
