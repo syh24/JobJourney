ae7c31622e442c8b01cedb2f3ad89198177d03f0
syh24 <dbsgur6896@naver.com>
2024-05-25 20:35:19 +0900

    :sparkles: 비디오 삭제 기능 추가

--- src/main/java/CSE4186/interview/service/VideoService.java
+++ src/main/java/CSE4186/interview/service/VideoService.java
@@ -4,6 +4,7 @@ import CSE4186.interview.controller.dto.VideoDto;
 import CSE4186.interview.entity.User;
 import CSE4186.interview.entity.Video;
 import CSE4186.interview.exception.NotFoundException;
+import CSE4186.interview.repository.PostVideoRepository;
 import CSE4186.interview.repository.UserRepository;
 import CSE4186.interview.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class VideoService {
 
     private final UserRepository userRepository;
+    private final PostVideoRepository postVideoRepository;
     private final VideoRepository videoRepository;
 
     @Transactional
@@ -42,5 +44,14 @@ public class VideoService {
                 .orElseThrow(() -> new NotFoundException("해당 유저가 존재하지 않습니다."));
         int page = pageable.getPageNumber() - 1;
         return videoRepository.findAllByUser(PageRequest.of(page, pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "createdAt")), findUser);
+    }
+
+    @Transactional
+    public void deleteVideo(Long id) {
+        Video video = videoRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("해당 비디오가 존재하지 않습니다."));
+
+        postVideoRepository.deleteByVideo(video);
+        videoRepository.deleteById(id);
     }
 }
